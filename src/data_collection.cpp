129b1a90a00e3a7585d714553c6120efd979cde1
drewfin42-spec <drewfin42@gmail.com>
2025-10-26 19:35:57 -0600

    Add Dibs comment for Drew F in data_collection.cpp

--- src/data_collection.cpp
+++ src/data_collection.cpp
@@ -3,4 +3,4 @@
 // Devices: BMP180 Barometer (used for sensing pressure and calculating height. See the docs on how to do that), MPU6050 accelerometer
 // Skills: Reading and implementing documentation (lots of resources are available, you just need to implment them!)
 
-// Dibs: 
+// Dibs: Drew F
